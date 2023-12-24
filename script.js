4b7e21cc029aade495f48520ad50d37eccbf66ee
Ben-Long50 <benjlong50@gmail.com>
2023-12-24 01:39:46 -0800

    added a predefined path to the maze

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const maze = (function() {
 
     let mazeCounter = 1;
+    let roomPosition1 = 0;
+    let roomPosition2 = 3;
 
     const rooms = {
         warmRoom: ['red', 'orange', 'yellow', 'death'],
@@ -14,26 +16,37 @@ const maze = (function() {
         doorThree: ''
     }
 
-    function getRandomRoom() {
-        return Math.floor(Math.random() * 4)
+    function countForwards() {
+        if(roomPosition1 === 4) {
+            roomPosition1 = 0;
+        }
+        return roomPosition1++;
+    }
+
+    function countBackwards() {
+        if(roomPosition2 === -1) {
+            roomPosition2 = 3;
+        }
+        return roomPosition2--;
     }
 
     function assignDoors() {
         if(mazeCounter % 2 !== 0 && mazeCounter % 3 !== 0) {
-            doors.doorOne = rooms.warmRoom[getRandomRoom()];
-            doors.doorTwo = rooms.warmRoom[getRandomRoom()];
-            doors.doorThree = rooms.warmRoom[getRandomRoom()];
+            doors.doorOne = rooms.warmRoom[countForwards()];
+            doors.doorTwo = rooms.warmRoom[countForwards()];
+            doors.doorThree = rooms.warmRoom[countForwards()];
         }
         else if(mazeCounter % 2 === 0) {
-            doors.doorOne = rooms.coldRoom[getRandomRoom()];
-            doors.doorTwo = rooms.coldRoom[getRandomRoom()];
-            doors.doorThree = rooms.coldRoom[getRandomRoom()];
+            doors.doorOne = rooms.coldRoom[countBackwards()];
+            doors.doorTwo = rooms.coldRoom[countBackwards()];
+            doors.doorThree = rooms.coldRoom[countBackwards()];
         }
         else if(mazeCounter % 3 === 0) {
-            doors.doorOne = rooms.monochromeRoom[getRandomRoom()];
-            doors.doorTwo = rooms.monochromeRoom[getRandomRoom()];
-            doors.doorThree = rooms.monochromeRoom[getRandomRoom()];
+            doors.doorOne = rooms.monochromeRoom[countForwards()];
+            doors.doorTwo = rooms.monochromeRoom[countBackwards()];
+            doors.doorThree = rooms.monochromeRoom[countForwards()];
         }
+        console.log(doors.doorOne, doors.doorTwo, doors.doorThree)
     }
 
     function advanceMazeCounter() {
@@ -54,7 +67,7 @@ const maze = (function() {
         if(chosenDoor === 'death') {
             domInteraction.openDeathDialog();
         }
-        else if(currentCount === 15) {
+        else if(currentCount === 30) {
             domInteraction.openWinDialog();
         }
     }
